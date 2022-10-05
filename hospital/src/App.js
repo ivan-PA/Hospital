0a256fbc291198c626c04c1550471447b2db2c08
Juan E <ivanpacha@hotmail.com>
2022-10-05 09:31:17 +0200

    Primer push

---
+++ hospital/src/App.js
@@ -0,0 +1,16 @@
+import Inicio from './componentes/Inicio.js';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-green-600 w-80 mx-auto text-center">
+        <Inicio/>
+      </h1>
+
+      <div className="container ">
+        <div className=""></div>
+
+      </div>
+    </div>
+  )
+}
---
+++ hospital/src/componentes/Inicio.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Inicio = () =>(
+<div className="input">
+    Hola Mundo
+  </div>
+);
+
+export default Inicio;
